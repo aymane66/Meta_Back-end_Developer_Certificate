e3244206c498680f165ac881f2133d42b4e744e9
Ayman MOUHCINE <120111126+aymane66@users.noreply.github.com>
2023-08-15 10:31:19 +0100

    Add files via upload

---
+++ Course 5 - Django/url dispatcher/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from http.client import HTTPResponse
+
+# Create your views here.
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment',
+        'lentals': 'type of food'
+    }
+
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2>{drink_name}</h2>" + choice_of_drink)
